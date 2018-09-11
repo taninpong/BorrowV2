e03235c41a60a339c08f23524bff107a8ef9b2b4
sirisak <sirisak022602020sunon@gmail.com>
2018-09-11 10:26:56 +0700

    add Model.ts

---
+++ BorrowV/src/app/Model.ts
@@ -0,0 +1,31 @@
+export class Getdata {
+
+    public id: string;
+    public nameitem: string;
+    public quantity: number;
+
+}
+
+export class ListitemSlotResponse {
+    public id: string;
+    public nameitem: string;
+    public quantity: number;
+    public total: number;
+
+}
+export class UserLogin{
+    static userlogin: string;  
+}
+
+// export class Insertitem{
+//     public id: string; 
+//     public nameitem: string;  
+//     public quantity: number;  
+//     public total: number;   
+// }
+
+// export class Slotitem{
+//     public id: string; 
+//     public slotname: string;  
+//     public item: Insertitem[];
+// }
