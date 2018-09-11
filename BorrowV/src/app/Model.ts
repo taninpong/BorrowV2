ec33a05e9f5b3caad57a4ae8d712f97daf540fa5
apiched1 <apiched_oh@hotmail.com>
2018-09-11 10:54:34 +0700

    add model

---
+++ BorrowV/src/app/Model.ts
@@ -0,0 +1,19 @@
+export class Getdata {
+
+    public id: string;
+    public nameitem: string;
+    public quantity: number;
+
+}
+
+export class ListitemSlotResponse {
+    public id: string;
+    public nameitem: string;
+    public quantity: number;
+    public total: number;
+
+}
+export class UserLogin{
+    static userlogin: string;  
+    
+}
